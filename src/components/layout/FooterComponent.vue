ebb664e2e4d638bc87495b61a1cf4456e6f2a236
paisible7 <22km127@esisalama.org>
2025-07-06 14:57:09 +0200

    fix(ui): theme logo & footer

--- src/components/layout/FooterComponent.vue
+++ src/components/layout/FooterComponent.vue
@@ -65,6 +65,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
               </svg>
               <a href="tel:+243992142932" class="dark-text-muted hover:text-capslock-navy dark:hover:text-blue-300 transition-colors">+243 992 142 932</a>
+              <a href="tel:+243994644422" class="dark-text-muted hover:text-capslock-navy dark:hover:text-blue-300 transition-colors">+243 994 644 422</a>
             </li>
           </ul>
         </div>
--- src/components/ui/AnimatedLogo.vue
+++ src/components/ui/AnimatedLogo.vue
@@ -9,13 +9,13 @@
 
     <!-- Logo pour le thème clair -->
     <img
-      src="@/assets/images/light.png"
+      src="@/assets/images/dark.png"
       alt="Logo clair"
       :class="`block dark:hidden w-full h-full object-contain ${isAnimating ? 'animate-pulse' : ''}`"
     />
     <!-- Logo pour le thème sombre -->
     <img
-      src="@/assets/images/dark.png"
+      src="@/assets/images/light.png"
       alt="Logo sombre"
       :class="`hidden dark:block w-full h-full object-contain ${isAnimating ? 'animate-pulse' : ''}`"
     />
